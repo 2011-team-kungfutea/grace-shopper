1f51eab9e69e81d1439ce030557cde41984013aa
Goncagul Ay <ggloria.ay@gmail.com>
2021-01-13 18:27:49 -0500

    BREAKING CHANGE: post route for the shopping is broken.

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Product} = require('../db/models')
+const {Product, Order, Order_Detail} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -10,3 +10,25 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/:productId', async (req, res, next) => {
+  try {
+    console.log(req.session)
+    const allOrders = await Order.findOrCreate({
+      where: {
+        userId: req.session.userId,
+        isOrdered: false
+      },
+      include: [{model: Order_Detail}],
+      defaults: {userId: req.session.userId, isOrdered: false}
+    })
+    //console.log(allOrders)
+    res.send(allOrders)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// var cart = req.body;
+//  req.session.cart = cart;
+//  req.session.save(function(err)
