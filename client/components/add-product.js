c91a81bc5f9da62b81b7f99300e54ce43c645775
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-14 20:08:41 -0500

    feat: completed add-product form

--- client/components/add-product.js
+++ client/components/add-product.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Form, Button, Checkbox, Input, Image} from 'semantic-ui-react'
+import {
+  Form,
+  Button,
+  Checkbox,
+  Input,
+  Image,
+  Container,
+  Header
+} from 'semantic-ui-react'
 
 export class AddProduct extends React.Component {
   constructor() {
@@ -19,26 +27,53 @@ export class AddProduct extends React.Component {
   render() {
     return (
       <div>
-        <h1>ADD PRODUCT</h1>
-        <Form>
-          <Form.Field>
-            <label>Product Name</label>
-            <Input name="name" placeholder="Product Name" />
-          </Form.Field>
-          <Form.Field>
-            <label>Image URL</label>
-            <Input
-              name="imageUrl"
-              value={this.state.imageUrl}
-              onChange={this.handleChange}
+        <Container textAlign="center">
+          <Header as="h1">Add Product</Header>
+        </Container>
+        <div className="add-product">
+          <Form className="add-product-form">
+            <Form.Field>
+              <label>Product Name</label>
+              <Input type="text" name="name" placeholder="Product Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Image URL</label>
+              <Input
+                name="imageUrl"
+                value={this.state.imageUrl}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Category</label>
+              <Input type="text" name="category" placeholder="Category" />
+            </Form.Field>
+            <Form.Field>
+              <label>Quantity</label>
+              <Input type="number" placeholder="Quantity" min="0" />
+            </Form.Field>
+            <Form.Field>
+              <label>Price</label>
+              <Input
+                type="number"
+                placeholder="Quantity"
+                min="0.01"
+                step="0.01"
+              />
+            </Form.Field>
+            <Button className="spacepurple" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <div className="image-preview">
+            <label>Image Preview</label>
+            <Image
+              className="add-product-image-preview"
+              src={this.state.imageUrl}
+              size="large"
             />
-          </Form.Field>
-          <Form.Field>
-            <label>Quantity</label>
-            <Input type="number" placeholder="Quantity" min="0" />
-          </Form.Field>
-        </Form>
-        <Image src={this.state.imageUrl} size="small" />
+          </div>
+        </div>
       </div>
     )
   }
