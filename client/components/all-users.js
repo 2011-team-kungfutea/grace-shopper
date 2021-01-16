ce4a0cf93933bf43615de23ff99750bd517e3bf8
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-15 23:24:18 -0500

    feat: adding all-users view

--- client/components/all-users.js
+++ client/components/all-users.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Table, Container, Header} from 'semantic-ui-react'
 import {fetchUsers} from '../store/all-users-reducer'
 
 class AllUsers extends React.Component {
@@ -11,9 +12,40 @@ class AllUsers extends React.Component {
     const {users} = this.props
     return (
       <div>
-        {users.map(user => {
-          return <div />
-        })}
+        <Container fluid>
+          <Header as="h2">All Users</Header>
+        </Container>
+        {/* collapsing - makes table shrink to contents */}
+        <Table definition striped selectable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell>First Name</Table.HeaderCell>
+              <Table.HeaderCell>Last Name</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+              <Table.HeaderCell>Address</Table.HeaderCell>
+              <Table.HeaderCell>Phone Number</Table.HeaderCell>
+              <Table.HeaderCell>Administrator</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {users.sort((a, b) => a.id - b.id).map(user => {
+              return (
+                <Table.Row key={user.id}>
+                  <Table.Cell>{user.id}.</Table.Cell>
+                  <Table.Cell>{user.firstName}</Table.Cell>
+                  <Table.Cell>{user.lastName}</Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell singleLine>{user.address}</Table.Cell>
+                  <Table.Cell>{user.phoneNumber}</Table.Cell>
+                  <Table.Cell>
+                    {user.isAdministrator ? <i className="check icon" /> : ''}
+                  </Table.Cell>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table>
       </div>
     )
   }
