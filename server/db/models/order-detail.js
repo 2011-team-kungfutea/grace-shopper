d0ea3201c44a4670c39656ad960ac8f616f7720d
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-20 12:02:20 -0500

    refactor: changing order_detail model to have price with BigInt type

--- server/db/models/order-detail.js
+++ server/db/models/order-detail.js
@@ -10,7 +10,7 @@ const Order_Detail = db.define('order_detail', {
     }
   },
   price: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.BIGINT,
     allowNull: false,
     validate: {
       min: 1
@@ -18,14 +18,14 @@ const Order_Detail = db.define('order_detail', {
   }
 })
 
-const checkQuantity = orderDetail => {
-  if (orderDetail.quantity > orderDetail.product.quantity) {
-    const err = new Error(`Too Many ${orderDetail.product}s.`)
-    throw err
-  }
-}
+// const checkQuantity = orderDetail => {
+//   if (orderDetail.quantity > orderDetail.product.quantity) {
+//     const err = new Error(`Too Many ${orderDetail.product}s.`)
+//     throw err
+//   }
+// }
 
-Order_Detail.beforeCreate(checkQuantity)
-Order_Detail.beforeUpdate(checkQuantity)
+// Order_Detail.beforeCreate(checkQuantity)
+// Order_Detail.beforeUpdate(checkQuantity)
 
 module.exports = Order_Detail
