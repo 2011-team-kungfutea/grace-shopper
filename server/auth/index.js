0873ec37d127b1cfe1a14ae49eeabbdbe1a1ec96
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-20 12:25:26 -0500

    refactor: adding error message for empty email field in signup form

--- server/auth/index.js
+++ server/auth/index.js
@@ -27,7 +27,10 @@ router.post('/signup', async (req, res, next) => {
     const user = await User.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
+    if (err.name === 'SequelizeValidationError') {
+      if (err.errors[0].path === 'email')
+        res.status(401).send('Email is required')
+    } else if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
     } else {
       next(err)
