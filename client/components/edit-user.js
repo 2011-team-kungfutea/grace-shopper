f4b3114cc0aac7b7fd1f3aa01e9a542dbaaf5f01
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2021-01-21 00:02:24 -0500

    feat: creating edit-users.js

--- client/components/edit-user.js
+++ client/components/edit-user.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {UserForm} from './user-form'
-import {me} from '../store/user'
+import {me, editMe} from '../store/user'
+import {Container, Header} from 'semantic-ui-react'
 
 class EditUser extends React.Component {
   constructor() {
@@ -23,8 +24,80 @@ class EditUser extends React.Component {
     const userId = this.props.match.params.userId
     this.props.getUser(userId)
   }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.user !== this.props.user) {
+      this.setState(this.props.user)
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    try {
+      const errors = this.validateForm()
+      if (!errors.length) {
+        this.props.updateUser(this.state)
+        this.setState({
+          errors: [],
+          submittedForm: 1
+        })
+      }
+    } catch (error) {
+      console.log('Unable to edit user', error)
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  validateForm() {
+    const errors = []
+    const {firstName, lastName, address, phoneNumber} = this.state
+    if (firstName === '' || firstName === null) {
+      errors.push('You must include a first name.')
+    }
+    if (lastName === '' || lastName === null) {
+      errors.push('You must include a last name.')
+    }
+    if (address === '' || address === null) {
+      errors.push('You must include an address.')
+    }
+    if (
+      phoneNumber === '' ||
+      phoneNumber === null ||
+      phoneNumber.length !== 10
+    ) {
+      errors.push('You must include a valid phone number.')
+    }
+    return errors
+  }
+
+  render() {
+    return (
+      <div>
+        <Container textAlign="center">
+          <Header as="h1">Edit My Account</Header>
+        </Container>
+        <UserForm
+          {...this.state}
+          formType="edited"
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
+    )
+  }
 }
 
-const mapDispatch = dispatch => ({
-  getUser: userId => dispatch(thunkfetchSingleUser(userId))
+const mapState = state => ({
+  user: state.singleUser
 })
+const mapDispatch = dispatch => ({
+  getUser: userId => dispatch(me(userId)),
+  updateUser: user => dispatch(editMe(user))
+})
+
+export default connect(mapState, mapDispatch)(EditUser)
