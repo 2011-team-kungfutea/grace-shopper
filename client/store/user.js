1e2ded3e9ba974b6f7b4008028d920653234d4f5
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2021-01-21 00:11:37 -0500

    feat: edit user redux

--- client/store/user.js
+++ client/store/user.js
@@ -6,6 +6,7 @@ import history from '../history'
  */
 const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
+const EDIT_USER = 'EDIT_USER'
 
 /**
  * INITIAL STATE
@@ -17,6 +18,7 @@ const defaultUser = {}
  */
 const getUser = user => ({type: GET_USER, user})
 const removeUser = () => ({type: REMOVE_USER})
+const editUser = user => ({type: EDIT_USER, user})
 
 /**
  * THUNK CREATORS
@@ -56,6 +58,17 @@ export const logout = () => async dispatch => {
   }
 }
 
+export const editMe = user => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`/auth/me/${user.id}`, user)
+      dispatch(getUser(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
 /**
  * REDUCER
  */
@@ -65,6 +78,8 @@ export default function(state = defaultUser, action) {
       return action.user
     case REMOVE_USER:
       return defaultUser
+    case EDIT_USER:
+      return action.user
     default:
       return state
   }
